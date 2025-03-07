473a879a91f3a321e2e21585699cb8cae2fac24d
Joyce Lau <jshizuki@yahoo.com.hk>
2025-03-07 11:38:25 +0900

    Initial enabling of IP blocklisting

---
+++ netlify/functions/block-ip.js
@@ -0,0 +1,24 @@
+exports.handler = async (event) => {
+
+  const blockedIps = [
+    "::1", // Block localhost for testing
+    "2001:f70:83e0:6000:1456:87f8:f059:b2c9", // IPv6 address
+    "122.219.239.178", // IPv4 address (in case it's used)
+  ];
+
+  const ipAddress =
+    event.headers["x-forwarded-for"] || event.headers["remote-addr"];
+
+  if (blockedIps.includes(ipAddress)) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ message: "Access forbidden" }),
+    };
+  }
+
+  // If the IP is not blocked, proceed to serve the site
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Access granted" }),
+  };
+};
